06287034567d97d77f2300adc572009b49b7bafd
JisungKim <rkdkawk22@naver.com>
2023-01-18 23:49:48 +0900

    Initial commit

---
+++ src/main/java/com/example/suejin/SuejinApplication.java
@@ -0,0 +1,21 @@
+package com.example.suejin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SuejinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuejinApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!!";
+	}
+
+}
---
+++ src/test/java/com/example/suejin/SuejinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suejin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuejinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
